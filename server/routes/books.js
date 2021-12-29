7c905335d9b24bf8397b9e742a50dc01a7695ab7
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2021-12-28 23:45:57 -0500

    Books CRUD API

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,30 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
+// Fake database
+const fakeDb = {
+  1: {
+    title: 'Dune',
+    author: 'Frank Herbert'
+  },
+  2: {
+    title: 'The Hobbit',
+    author: 'JRR Tolkien'
+  }
+};
+
+
 /* List all books */
 router.get('/', function(req, res, next) {
-  res.send('List all books');
+  const booksArray = Object.values(fakeDb);
+  res.send(booksArray);
 });
 
 /* Create book */
 router.post('/', function(req, res, next) {
-  res.send(`create new book`);
+  const arr = Object.keys(fakeDb);
+  const nextId = parseInt(arr[arr.length -1]) +1;
+
+  const newBook = req.body;
+  fakeDb[nextId] = newBook;
+
+  res.send(newBook);
 });
 
 /* Read book */
 router.get('/:id', function(req, res, next) {
-  console.log('...', req);
-  res.send(`read book # ${req.params.id}`);
+  const id = req.params.id;
+  const book = fakeDb[id];
+  res.send(book);
 });
 
 /* Update book */
 router.put('/:id', function(req, res, next) {
-  res.send(`update book # ${req.params.id}`);
+  const id = req.params.id;
+  const newBook = req.body;
+  fakeDb[id] = newBook;
+  res.send(newBook);
 });
 
 /* Delete book */
 router.delete('/:id', function(req, res, next) {
-  res.send(`delete book # ${req.params.id}`);
+  const id = req.params.id;
+  const book = fakeDb[id];
+  delete fakeDb[id];
+  res.send(book);
 });
 
 module.exports = router;
