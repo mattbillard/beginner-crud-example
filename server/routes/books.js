86266d262d57218a8042b53c3a4525844174e6ae
Matt Billard <2836258+mattbillard@users.noreply.github.com>
2021-12-28 23:20:34 -0500

    Create books API

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* List all books */
 router.get('/', function(req, res, next) {
-  res.send('list the books testing');
+  res.send('List all books');
+});
+
+/* Create book */
+router.post('/', function(req, res, next) {
+  res.send(`create new book`);
+});
+
+/* Read book */
+router.get('/:id', function(req, res, next) {
+  console.log('...', req);
+  res.send(`read book # ${req.params.id}`);
+});
+
+/* Update book */
+router.put('/:id', function(req, res, next) {
+  res.send(`update book # ${req.params.id}`);
+});
+
+/* Delete book */
+router.delete('/:id', function(req, res, next) {
+  res.send(`delete book # ${req.params.id}`);
 });
 
 module.exports = router;
